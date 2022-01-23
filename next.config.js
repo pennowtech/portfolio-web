93e9d05c83b0aea557620f2c31be8f47da62b2c1
Sukhdeep Singh <sukhdeep.singh@agile-robots.com>
2022-01-23 19:42:39 +0100

    Error: Image Optimization using Next.js default loader is not compatible with 'next export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
+  //https://stackoverflow.com/a/70047180
   images: {
     domains: ['www.nasa.gov', 'pennow.tech', 'pixabay.com', 'www.google.com', 'wordpress-561320-2383780.cloudwaysapps.com'],
+    loader: 'akamai',
+    path: '',
   },
   reactStrictMode: true,
 
