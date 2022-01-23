23a264508809312b2c8942336a6b5362a50ae4b7
Sukhdeep Singh <sukhdeep.singh@agile-robots.com>
2022-01-23 18:38:42 +0100

    dummy change

--- pages/index.js
+++ pages/index.js
@@ -5,31 +5,31 @@ import 'tailwindcss/tailwind.css'
 import { Navbar } from '../components/Navbar';
 
 const Home = () => {
-    return (
-        <div className='w-full'>
-        <div className=' mx-auto min-h-screen md:max-w-screen-lg xl:max-w-[1167px] shadow-sm '>
-            <Head>
-                <title>My Personal Blog</title>
-                <meta name='description' content='My personal Blog on modern software technologies' />
-                <link rel='icon' href='/favicon.ico' />
-            </Head>
-            <Navbar />
-            <h1 className="text-3xl font-bold underline items-center text-green-500">
-                Hello world!
-            </h1>
-            <Element name="Market" className="element min-h-[1500px]" >
-            test 1
+  return (
+    <div className='w-full'>
+      <div className=' mx-auto min-h-screen md:max-w-screen-lg xl:max-w-[1167px] shadow-sm '>
+        <Head>
+          <title>My Personal Blog</title>
+          <meta name='description' content='My personal Blog on modern software technologies' />
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <Navbar />
+        <h1 className="text-3xl font-bold underline items-center text-green-500">
+          Hello world!
+        </h1>
+        <Element name="Market" className="element min-h-[1500px]" >
+          test 1
         </Element>
 
         <Element name="Exchange" className="element min-h-[1500px]">
-            test 2
+          test 2
         </Element>
         <Element name="Tutorials" className="element min-h-[1500px]">
-            test 3
+          test 4
         </Element>
-        </div>
-        </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Home;
