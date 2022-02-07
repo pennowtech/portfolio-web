ce57338497d68993ecde452613a96fb8470729bb
Sukhdeep Singh <sukhdeep.singh@agile-robots.com>
2022-02-07 13:38:58 +0100

    feat: moving posts from old pennow.tech website to this website

--- utils/persistTheme.jsx
+++ utils/persistTheme.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line max-len
 if (localStorage.getItem('pntDarkTheme') === 'true' || (!('pntDarkTheme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
   document.documentElement.classList.add('dark');
   document.documentElement.style.cssText = 'color-scheme:dark';
