dcd34f42977f397168022f04db6a2e60ae2c84f0
Sukhdeep Singh <sdsingh.developer@gmail.com>
2023-05-04 02:35:33 +0200

    🚀 build: Disabled linting during build

--- next.config.js
+++ next.config.js
@@ -17,6 +17,11 @@ module.exports = removeImports({
     loader: 'akamai',
     path: ''
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true
+  },
   reactStrictMode: true,
   serverRuntimeConfig: {
     PROJECT_ROOT: __dirname
